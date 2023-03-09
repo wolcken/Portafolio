0226284be9039e6cab34b635d037d39649385b5a
Wolcken_Git <Alfredoxd00@hotmail.com>
2023-03-09 12:54:26 -0400

    Customs V2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import '../styles/header.css';
-// import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-// import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import { NavLink } from 'react-router-dom';
 
@@ -29,30 +26,10 @@ const Header = () => {
                             </Offcanvas.Header>
                             <Offcanvas.Body>
                                 <Nav className="justify-content-end flex-grow-1 pe-3">
-                                    <Nav.Link> <NavLink to={'/'}>Home</NavLink> </Nav.Link>
-                                    <Nav.Link> <NavLink to={'/projects'}>Projects</NavLink> </Nav.Link>
-                                    <NavDropdown
-                                        title="About"
-                                        drop='start'
-                                        id={`offcanvasNavbarDropdown-expand-${expand}`}
-                                    >
-                                        <NavDropdown.Item> <NavLink to={'/contact'}>Contact</NavLink> </NavDropdown.Item>
-                                        <NavDropdown.Item disabled> <NavLink to={'/hobbies'}>Hobbies</NavLink> </NavDropdown.Item>
-                                        <NavDropdown.Divider />
-                                        <NavDropdown.Item >
-                                            <NavLink to={'/studies'}>Studies</NavLink>
-                                        </NavDropdown.Item>
-                                    </NavDropdown>
+                                    <NavLink to={'/projects'}>Projects</NavLink>
+                                    <NavLink to={'/studies'}>Studies</NavLink>
+                                    <NavLink to={'/contact'}>Contact</NavLink>
                                 </Nav>
-                                {/* <Form className="d-flex">
-                                    <Form.Control
-                                        type="search"
-                                        placeholder="Search"
-                                        className="me-2"
-                                        aria-label="Search"
-                                    />
-                                    <Button variant="outline-secondary">Search</Button>
-                                </Form> */}
                             </Offcanvas.Body>
                         </Navbar.Offcanvas>
                     </Container>
